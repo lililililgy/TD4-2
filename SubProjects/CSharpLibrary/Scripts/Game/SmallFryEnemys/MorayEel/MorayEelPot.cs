f7ca3cda2c4929747e5b9ea761f744e18e6fa945
ku-rihara <k023g0027@g.neec.ac.jp>
2026-06-19 15:03:05 +0900

    MorayEelクラス追加

---
+++ SubProjects/CSharpLibrary/Scripts/Game/SmallFryEnemys/MorayEel/MorayEelPot.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MorayEelPot : MonoScript {
+	/* ----- 移動に用いる変数群 ----- */
+	[SerializeField] private bool isActive_;
+
+	public override void Initialize() {
+	
+	}
+
+	public override void Update() {
+		
+	}
+}
