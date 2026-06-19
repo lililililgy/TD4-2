888ba28ec5c396910539fd9b69037c1dd5c592ca
OriharaIssei <k023g0048@g.neec.ac.jp>
2026-06-19 16:18:38 +0900

    Player移動 完成

--- SubProjects/CSharpLibrary/Scripts/Game/Player/PlayerMoveComponent.cs
+++ SubProjects/CSharpLibrary/Scripts/Game/Player/PlayerMoveComponent.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,36 +15,55 @@ public class PlayerMoveComponent
         Transform transform = entity.GetComponent<Transform>();
         PlayerInputComponent inputComp = entity.GetScript<PlayerInputComponent>();
 
-        Vector3 moveDir;
+        float cullRoll = 0f;
+        float prevRoll = 0f;
         // 減衰
-        if (inputComp.moveDir_.Length() == 0) {
-            velocity_ = SpringDamper.SmoothDamp<float, FloatDampTraits>(
-                velocity_, 0.0f, ref currentDecel_, smoothTime_, Time.deltaTime, maxSmoothSpeed_);
+        if (inputComp.moveDir_.Length() <= 0f) {
+            prevRoll = Mathf.Atan2(velocity_.x, velocity_.y);
 
-            moveDir = Quaternion.RotateVector(transform.rotate, Vector3.forward);
+            const float kThresholdSpeed = 0.1f;
 
-        } else {
-            // 加速
-            velocity_ += accel_ * Time.deltaTime;
-            if (velocity_ > maxSpeed_) {
-                velocity_ = maxSpeed_;
+            if (velocity_.Length() < kThresholdSpeed) {
+                velocity_ = Vector3.zero;
+                return;
             }
 
-            moveDir = new Vector3(inputComp.moveDir_.x, inputComp.moveDir_.y, 0.0f);
-        }
-        
-        moveDir = moveDir.Normalized();
+            velocity_ = SpringDamper.SmoothDamp<Vector3, Vector3DampTraits>(
+                velocity_, Vector3.zero, ref currentDecelSmoothSpeed_, decelSmoothTime_, Time.deltaTime, decelMaxSmoothSpeed_);
 
-        transform.rotate = Quaternion.LookAt(Vector3.zero, moveDir, Vector3.up);
-        transform.position += moveDir * velocity_ * Time.deltaTime;
+            cullRoll = Mathf.Atan2(velocity_.x, velocity_.y);
+
+        } else {
+            Vector3 moveDir = new Vector3(inputComp.moveDir_.x, inputComp.moveDir_.y, 0.0f);
+
+            cullRoll += Mathf.Atan2(moveDir.x, moveDir.y);
+            prevRoll = Mathf.Atan2(velocity_.x, velocity_.y);
+
+            // 加速
+            velocity_ += moveDir * (accel_ * Time.deltaTime);
+            if (velocity_.Length() > maxSpeed_) {
+                velocity_ = velocity_.Normalized() * maxSpeed_;
+            }
+        }
+
+
+        transform.rotate = Quaternion.MakeFromAxis(Vector3.back, SpringDamper.SmoothDamp<float, FloatDampTraits>(
+            cullRoll, prevRoll, ref currentRotateSmoothSpeed_, currentRotateSmoothTime_, Time.deltaTime, currentRotateMaxSmoothSpeed_));
+
+        transform.position += velocity_ * Time.deltaTime;
     }
 
-    [SerializeField] private float accel_ = 5.0f;
-    [SerializeField] private float velocity_ = 0.0f;
+    private float currentRotateSmoothSpeed_ = 0.0f;
+    [SerializeField] private float currentRotateSmoothTime_ = 0.0f;
+    [SerializeField] private float currentRotateMaxSmoothSpeed_ = 0.0f;
 
-    private float currentDecel_ = 0.0f;
-    [SerializeField] private float smoothTime_ = 0.1f;
-    [SerializeField] private float maxSmoothSpeed_ = 1000.0f;
+
+    [SerializeField] private float accel_ = 5.0f;
+    [SerializeField] private Vector3 velocity_ = new Vector3(0.0f, 0.0f, 0.0f);
+
+    private Vector3 currentDecelSmoothSpeed_ = Vector3.zero;
+    [SerializeField] private float decelSmoothTime_ = 0.1f;
+    [SerializeField] private float decelMaxSmoothSpeed_ = 1000.0f;
 
     [SerializeField] private float maxSpeed_ = 36.0f;
 
