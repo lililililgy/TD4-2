9efb58cee9ddf4e887ba804cc13b90811fc11a4e
OonoYouji <youji.o.0405@gmail.com>
2026-07-03 17:18:40 +0900

    uiに動き

--- SubProjects/CSharpLibrary/Scripts/Game/UI/Test/UITest1.cs
+++ SubProjects/CSharpLibrary/Scripts/Game/UI/Test/UITest1.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 
 public class UITest1 : MonoScript {
 
+	bool isSelect = false;
+	Vector3 savePos;
+	float time = 0f;
+
+	public override void Initialize() {
+		savePos = transform.position;
+	}
+
+	public override void Update() {
+		if (isSelect) {
+			float posY = Mathf.Sin(SteppedTimeEasing(Time.time * 3f, 1)) * 0.1f;
+			transform.position = savePos + new Vector3(0, posY, 0);
+		}
+	}
+
 
 	public void OnSelect() {
 		SpriteRenderer renderer = entity.GetComponent<SpriteRenderer>();
-		if(renderer) {
+		if (renderer) {
 			renderer.color = Vector4.red;
 		}
+		isSelect = true;
 	}
 
 	public void OnDeselect() {
@@ -16,6 +32,14 @@ public class UITest1 : MonoScript {
 		if (renderer) {
 			renderer.color = Vector4.one;
 		}
+		isSelect = false;
+	}
+
+
+	public static float SteppedTimeEasing(float t, int steps) {
+		// 進行度tを階段状にする
+		float steppedT = Mathf.Floor(t * steps) / steps;
+		return steppedT;
 	}
 
 }
