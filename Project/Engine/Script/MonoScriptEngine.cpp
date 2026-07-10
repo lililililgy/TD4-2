470da6c3d28e2d57f8c172c39181a31ae7879d67
OonoYouji <youji.o.0405@gmail.com>
2026-07-10 11:01:22 +0900

    feat(mono-dbg-reattach-fix): auto-commit before merge

--- Project/Engine/Script/MonoScriptEngine.cpp
+++ Project/Engine/Script/MonoScriptEngine.cpp
@@ -1216,3 +1216,52 @@ bool MonoScriptEngine::BuildCSharpProject(std::string& outMessage) {
 	return (exitCode == 0);
 }
 
+void MonoScriptEngine::UpdateDebuggerStatus() {
+#if defined(DEBUG_MODE)
+	bool currentAttached = mono_is_debugger_attached() ? true : false;
+	if (currentAttached && !wasDebuggerAttached_) {
+		Console::Log("[Mono] Debugger newly attached! Syncing breakpoints by refreshing AppDomain...", LogCategory::ScriptEngine);
+		
+		if (assembly_) {
+			Console::Log("[Mono] Triggering fast reload for debugger synchronization...", LogCategory::ScriptEngine);
+			
+			MonoDomain* oldDomain = domain_;
+			domain_ = CreateReloadDomain();
+			mono_domain_set(domain_, true);
+
+			// 現在使用している DLL パスからロード
+			assembly_ = LoadAssemblyWithSymbols(domain_, currentDllPath_);
+			if (assembly_) {
+				image_ = mono_assembly_get_image(assembly_);
+				RegisterFunctions();
+
+				// ComponentBatchManagerの再初期化
+				MonoClass* batchMgrClass = mono_class_from_name(image_, "", "ComponentBatchManager");
+				if(batchMgrClass) {
+					MonoMethod* initMethod = mono_class_get_method_from_name(batchMgrClass, "Initialize", 0);
+					if(initMethod) {
+						MonoObject* exc = nullptr;
+						MonoScriptEngineUtils::SafeInvoke(initMethod, nullptr, nullptr, &exc);
+						if(exc) MonoScriptEngineUtils::HandleException(exc);
+					}
+				}
+
+				if (oldDomain != rootDomain_) {
+					domainsToUnload_.push_back(oldDomain);
+				}
+
+				SetIsHotReloadRequest(true);
+				Console::Log("[Mono] Debugger synchronization complete. Breakpoints should now bind.", LogCategory::ScriptEngine);
+			} else {
+				// フォールバック
+				mono_domain_set(oldDomain, true);
+				mono_domain_unload(domain_);
+				domain_ = oldDomain;
+				Console::LogError("[Mono] Failed to reload assembly for debugger sync.", LogCategory::ScriptEngine);
+			}
+		}
+	}
+	wasDebuggerAttached_ = currentAttached;
+#endif
+}
+
--- Project/Engine/Script/MonoScriptEngine.h
+++ Project/Engine/Script/MonoScriptEngine.h
@@ -116,6 +116,9 @@ public:
 	
 	void UpdateAiIntents(void* data, int count, float deltaTime, const std::string& groupName);
 
+	// デバッガ接続状態の監視と自動リロード
+	void UpdateDebuggerStatus();
+
 	/// @brief C#のBlackboardManagerにイベント完了を通知する
 	/// @param entityId 対象のエンティティID
 	/// @param eventName 完了したイベント名
@@ -170,6 +173,7 @@ private:
 	MonoClassField* sceneNameField_ = nullptr;
 
 	EntityComponentSystem* pEcs_ = nullptr;
+	bool wasDebuggerAttached_ = false;
 
 public:
 	/// ===================================================
