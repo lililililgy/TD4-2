abe34d3ffe78c45651ab40bd12619221b96d7c76
OonoYouji <youji.o.0405@gmail.com>
2026-07-14 02:46:45 +0900

    feat(text-renderer-inspector-multiline-10lines): auto-commit before merge

--- Project/Engine/ECS/Component/Components/RendererComponents/Text/TextRenderer.cpp
+++ Project/Engine/ECS/Component/Components/RendererComponents/Text/TextRenderer.cpp
@@ -27,7 +27,7 @@ void ComponentDebug::TextDebug(TextRenderer* tr, Asset::AssetCollection* assetCo
 	// 簡単なデバッグUI (複数行テキスト入力)
 	char textBuf[1024];
 	strncpy(textBuf, tr->GetText().c_str(), sizeof(textBuf));
-	if (ImGui::InputTextMultiline("Text", textBuf, sizeof(textBuf), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 4.0f))) {
+	if (ImGui::InputTextMultiline("Text", textBuf, sizeof(textBuf), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 10.0f))) {
 		tr->SetText(textBuf);
 	}
 
