6f3686bf11abbe81042f5b64197b04ae6bfb89e4
OonoYouji <youji.o.0405@gmail.com>
2026-07-15 11:12:34 +0900

    CollisionTest

--- SubProjects/CSharpLibrary/Scripts/Test/ColliderTest1.cs
+++ SubProjects/CSharpLibrary/Scripts/Test/ColliderTest1.cs
@@ -3,32 +3,45 @@ using System.Collections.Generic;
 
 
 
-struct TestStruct {
+struct TestStruct
+{
 	[SerializeField] float test;
 };
 
 
 
-public class ColliderTest1 : MonoScript {
+public class ColliderTest1 : MonoScript
+{
 
 	[SerializeField] float test = 0.0f;
 	[SerializeField] TestStruct testStruct;
 
-	public override void Initialize() {
-		
+	public override void Initialize()
+	{
+
 	}
 
-	public override void Update() {
+	public override void Update()
+	{
 		SpriteRenderer renderer = entity.GetComponent<SpriteRenderer>();
-		if(renderer) {
+		if (renderer)
+		{
 			renderer.color = Vector4.one;
 		}
 	}
 
 
-	public override void OnCollisionStay(Entity entity) {
+	public override void OnCollisionEnter(Entity collision)
+	{
+		collision.Destroy();
+	}
+
+
+	public override void OnCollisionStay(Entity entity)
+	{
 		SpriteRenderer renderer = entity.GetComponent<SpriteRenderer>();
-		if (renderer) {
+		if (renderer)
+		{
 			renderer.color = Vector4.red;
 		}
 	}
