534598b6b62ee145e55fed939b561f822f4e95d9
OonoYouji <youji.o.0405@gmail.com>
2026-07-21 23:39:06 +0900

    最後に入力したのがkeyboard or gamepadの判定を取る

--- SubProjects/CSharpLibrary/Scripts/Game/UI/Game/LastInputType.cs
+++ SubProjects/CSharpLibrary/Scripts/Game/UI/Game/LastInputType.cs
@@ -8,35 +8,87 @@ using System.Collections.Generic;
 public class LastInputType : MonoScript
 {
 
+	public enum InputType
+	{
+		Keyboard = 0,
+		Gamepad = 1
+	}
+
 	int lastInputType = 0;
+	Entity gamepadEntity = null;
+	Entity keyboardEntity = null;
 
 	public override void Initialize()
 	{
 
+		gamepadEntity = entity.GetChild(1);
+		keyboardEntity = entity.GetChild(0);
+
+		if (gamepadEntity)
+		{
+			gamepadEntity.enable = false;
+		}
+
+		if (keyboardEntity)
+		{
+			keyboardEntity.enable = true;
+		}
 	}
 
 	public override void Update()
 	{
 
+		int prevInputType = lastInputType;
+		lastInputType = GetLastInputType();
+
+		if (lastInputType != -1)
+		{
+			if (lastInputType != prevInputType)
+			{
+				switch (lastInputType)
+				{
+					case (int)InputType.Keyboard:
+						if (keyboardEntity)
+						{
+							keyboardEntity.enable = true;
+						}
+						if (gamepadEntity)
+						{
+							gamepadEntity.enable = false;
+						}
+						break;
+
+					case (int)InputType.Gamepad:
+						if (keyboardEntity)
+						{
+							keyboardEntity.enable = false;
+						}
+						if (gamepadEntity)
+						{
+							gamepadEntity.enable = true;
+						}
+						break;
+				}
+			}
+		}
+
 	}
 
 
 
-	int LastInputType()
+	int GetLastInputType()
 	{
 		if (Input.PressAnyKey())
 		{
-			lastInputType = 0;
-			return lastInputType;
+			return (int)InputType.Keyboard;
 		}
 
-		if (Input.PressGamepadAny())
+		if (Input.PressAnyGamepad())
 		{
-			lastInputType = 1;
-			return lastInputType;
+			return (int)InputType.Gamepad;
 		}
 
-		return lastInputType;
+		return -1;
 	}
 
 }
