05a51a1581c14e9dc9eaf88b850a0e6c8a2b8724
OonoYouji <youji.o.0405@gmail.com>
2026-07-23 02:13:02 +0900

    escapeでpauseを閉じたらplayerが動かなくなるバグの修正

--- SubProjects/CSharpLibrary/Scripts/Game/UI/Pause/PauseGroup.cs
+++ SubProjects/CSharpLibrary/Scripts/Game/UI/Pause/PauseGroup.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
 
-public class PauseGroup : MonoScript {
+public class PauseGroup : MonoScript
+{
 
 	[SerializeField] public float time = 0f;
 	[SerializeField] public float speed = 1.2f;
@@ -12,13 +13,17 @@ public class PauseGroup : MonoScript {
 	/// <summary>
 	///  pause sceneにおいてキャンセルボタンが押されたとき
 	/// </summary>
-	public void OnCancel() {
+	public void OnCancel()
+	{
+		SceneManager.SetUpdatePaused("GameScene", false);
 		SceneManager.Unload("PauseScene");
 	}
 
-	public void OnUISelect(string selectElementName) {
+	public void OnUISelect(string selectElementName)
+	{
 		SEPlayer audio = entity.GetComponent<SEPlayer>();
-		if(audio) {
+		if (audio)
+		{
 			audio.Play();
 		}
 	}
